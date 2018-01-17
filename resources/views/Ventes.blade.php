375c16bd734638bf1a48e660f9cfb3a757cd1255
Khaled Djhehiche <khaled.djhehiche@Digital-Grenoble.local>
2018-01-17 10:09:41 +0100

    test

--- resources/views/Ventes.blade.php
+++ resources/views/Ventes.blade.php
@@ -83,8 +83,12 @@
                 </div>
 
                 <div class="links">
-                    <a href = "/">Machineacafe</a>
+                    <a href="index">Home</a>
+                    <a href="selectDrink">Selection des boissons</a>
                     <a href="recettes">Liste des Recettes</a>
+                    <a href="ingredients">Liste des Ingrédients</a>
+                    <a href="ventes">Liste des Ventes</a>
+                </div>
                 </div>
             </div>
         </div>
