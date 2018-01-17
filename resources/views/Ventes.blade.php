e4a58df605900b7a559359cd1eb714d400b0f3c0
Veronique Rouault <veronique.rouault@Digital-Grenoble.local>
2018-01-17 17:40:19 +0100

    modification du tableau des ventes

--- resources/views/Ventes.blade.php
+++ resources/views/Ventes.blade.php
@@ -11,20 +11,20 @@
 @section('content')
     <div class="container">
         <table class="table table-hover">
-                <thead>
-                    <tr>
-                        @foreach ($RetourTab as $typeVente => $donneeVente)
-                        <th>{{ $typeVente}}</th>
-                        @endforeach  
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        @foreach ($RetourTab as $typeVente => $donneeVente)
-                        <td>{{ $donneeVente }}</td>
-                        @endforeach
-                    </tr> 
-                </tbody>
+            <thead>
+                <tr>
+                    @foreach ($RetourTab as $typeVente => $donneeVente)
+                    <th>{{ $typeVente}}</th>
+                    @endforeach  
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    @foreach ($RetourTab as $typeVente => $donneeVente)
+                    <td>{{ $donneeVente }}</td>
+                    @endforeach
+                </tr> 
+            </tbody>
 
 
                 {{--  <thead>
