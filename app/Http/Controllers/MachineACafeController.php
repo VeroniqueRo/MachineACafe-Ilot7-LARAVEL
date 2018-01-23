7469012ca3a038e651fb8aba6d8bb8692cfae9e8
Thomas <thomas.francese@campus-numerique-in-the-alps.com>
2018-01-23 17:01:26 +0100

    ajout affichage prix et nom boisson

--- app/Http/Controllers/MachineACafeController.php
+++ app/Http/Controllers/MachineACafeController.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class machineACafeController extends Controller
 {
-function listDrink() {
-    $drinkChoice =['Café au lait' => 60, 'Thé' => 70, 'Expresso' => 40, 'Café long' => 50];
+  function listDrink()
+  {
+    $drinkChoice = DB::select('select nomBoisson, codeBoisson from boisson');
+      return view('machineACafe', ['boissons' => $drinkChoice]);
+  }
 
-     return view('machineACafe', compact('drinkChoice'));
-
- }
+  function showDrink($code)
+  {
+    $drink = DB::select('select nomBoisson, prix from boisson where codeBoisson = ?',[$code]);
+    return view('boisson.affichageBoisson',['detailBoisson' => $drink]);
+  }
 }
 ?>
