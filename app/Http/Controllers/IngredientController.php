d166b650b11e5fdf175c154df6ff34c93ceb15a7
Romain Joubert <romain.joubert@Digital-Grenoble.local>
2018-01-23 09:57:26 +0100

    Intégration liste des ingrédients dans blade et dans le controller

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers;
 class IngredientController extends Controller
 {
   function listIngredients() {
+  	$ingredients = array(
+  		'Café' => 100,
+  		'Eau'=> 100,
+  		'Thé'  => 100,
+  		'Lait' => 100,
+      'Sucre' => 100
+  		);
 
-     return view('ingredients');
+
+     return view('ingredients', compact('ingredients'));
  }
 }
 ?>
