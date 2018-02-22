773af0c52ec592e91c589e2056ca731689731893
Veronique Rouault <veronique.rouault@Digital-Grenoble.local>
2018-02-22 11:45:07 +0100

    correction menu accueil + taille titres

--- resources/views/template/menu.blade.php
+++ resources/views/template/menu.blade.php
@@ -9,55 +9,49 @@
             <span class="icon-bar"></span>
         </button>
         <!-- Branding image -->
-        <img width="50em" height="50em" src="../img/coffee.png"><a class="navbar-brand" href="{{ url('/') }}"> La Pause S'Impose</a>      </div>
+        <img width="50em" height="50em" src="../img/coffee.png"><a class="navbar-brand" href="{{ url('/') }}"> La Pause S'Impose</a>
+      </div>
       <div class="collapse navbar-collapse" id="app-navbar-collapse">
         <!-- Left Side Of Navbar -->
         <ul class="nav navbar-nav navbar-left">
+          @if (Illuminate\Support\Facades\Auth::check())
+              {{--  @if (Illuminate\Support\Facades\Auth::user()->role=== 'admin')  --}}
+              <li class="active"><a href="../">Accueil</a></li>
+              <li><a href="{{ url('/machineACafe')}}">Machine à Café</a></li>
+              <li><a href="{{ route('listeBoissons')}}">Boissons</a></li>
+              <li><a href="{{ route('listeIngredients')}}">Ingrédients</a></li>
+              <li><a href="{{ route('listeRecettes')}}">Recettes</a></li>
+              <li><a href="{{ url('/gestionMonnaie')}}">Gestion Monnaie</a></li>
+              {{--  @endif  --}}
+              <li><a href="{{ route('listeVentes')}}">Ventes</a></li>
+              <!-- Right Side Of Navbar -->
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                    aria-expanded="false" aria-haspopup="true">
+                    Compte de {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="{{ route('logout') }}"
+                          onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+                          Déconnexion
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                              style="    display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                    <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span> Nouveau compte</a></li>
+                </ul>
+              </li>
+          @else
             <li class="active"><a href="../">Accueil</a></li>
             <li><a href="{{ url('/machineACafe')}}">Machine à Café</a></li>
-            @if (Illuminate\Support\Facades\Auth::check())
-                {{--  @if (Illuminate\Support\Facades\Auth::user()->role=== 'admin')  --}}
-                <li><a href="{{ route('listeBoissons')}}">Boissons</a></li>
-                <li><a href="{{ route('listeIngredients')}}">Ingrédients</a></li>
-                <li><a href="{{ route('listeRecettes')}}">Recettes</a></li>
-                <li><a href="{{ url('/gestionMonnaie')}}">Gestion Monnaie</a></li>
-                {{--  @endif  --}}
-                <li><a href="{{ route('listeVentes')}}">Ventes</a></li>
-            @endif
+            <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Connexion</a></li>
+          @endif
         </ul>
-        <!-- Right Side Of Navbar -->
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-          <!-- Right Side Of Navbar -->
-          <ul class="nav navbar-nav navbar-right">
-              <!-- Authentication Links -->
-              @guest
-                <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Connexion</a></li>
-                <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span> Enregistrez-vous</a></li>
-              @else
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                      aria-expanded="false" aria-haspopup="true">
-                      Bonjour {{ Auth::user()->name }} <span class="caret"></span>
-                  </a>
-                  <ul class="dropdown-menu">
-                      <li>
-                          <a href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                            Logout
-                          </a>
-                          <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                style="    display: none;">
-                              {{ csrf_field() }}
-                          </form>
-                      </li>
-                  </ul>
-                </li>
-              @endguest
-          </ul>
-        </div>
       </div>
     </div>
-  </div>
 </nav>
 <script src="{{ asset('js/app.js') }}"></script>
