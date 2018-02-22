fc8775aa2747c3cb978742219228f31645638a29
Veronique Rouault <veronique.rouault@Digital-Grenoble.local>
2018-02-22 10:04:51 +0100

    correction merge

--- app/Http/Controllers/RecetteController.php
+++ app/Http/Controllers/RecetteController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Http\Controllers;
@@ -105,41 +104,6 @@ class RecetteController extends Controller
 
 }
 ?>
-=======
-	<?php
-	namespace App\Http\Controllers;
-
-	class RecetteController extends Controller
-	{
-	  	function listRecettes() {
-	  	$recette = array(
-					'Expresso'	=> array(
-									'café'=>1,
-									'eau'=>1),
-					'Café_long'	=>array
-									(
-									'café'=>2,
-									'eau'=>2
-									),
-					'Thé'		=>array
-									(
-									'thé'=>1,
-									'eau'=>2
-									),
-					'Café au lait'=>array
-									(
-									'café'=>1,
-									'eau'=>2,
-									'lait'=>2
-									),
-	  	            );
-
-
-	     return view('recettes',compact('recette'));
-		}
-	}
-	?>
->>>>>>> 5f30d2323a5f26886195859324ba735aa7337688
 
 	    
 
